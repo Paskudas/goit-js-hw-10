505ea33ee98d48c6dd3973b2d22734a93ca64fc8
Paskudas <77464661+Paskudas@users.noreply.github.com>
2023-06-13 13:52:59 +0300

    +style

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import SlimSelect from 'slim-select'
+import { Notify } from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const breedSelect = document.querySelector('.breed-select');
@@ -40,12 +42,15 @@ breedSelect.addEventListener('change', () => {
 
   fetchCatByBreed(selectedBreedId)
     .then(cat => {
-      catInfo.innerHTML = `
+      catInfo.innerHTML = `<div class="thumb">
         <img src="${cat.url}" alt="Cat Image" />
-        <h2>${cat.breeds[0].name}</h2>
-        <p>${cat.breeds[0].description}</p>
-        <p>Temperament: ${cat.breeds[0].temperament}</p>
-      `;
+        </div><div class='description'><h1>${cat.breeds[0].name}</h1>
+        <p class='description-text'>
+        <span><b>Description: </b>${cat.breeds[0].description}</span>
+      </p>        
+      <p class='description-temperament'>
+      <span><b>Temperament: </b>${cat.breeds[0].temperament}</span></p>
+        </div>`;
 
       loader.style.display = 'none';
       catInfo.style.display = 'block';
